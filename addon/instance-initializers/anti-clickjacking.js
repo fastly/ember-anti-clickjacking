700a230da62c8bebaa68ac61e7a93088d7f48d27
James A Rosen <james@fastly.com>
2017-08-25 09:23:49 -0700

    add <style> defense
     * by default, add a `<style>` tag to `<head>` that hides the entire page
     * in the instance initializer, remove the `<style>` tag if
       `self === top`
     * allow apps to turn off the `<style>` tag injection because it interacts
       poorly with `<noscript>` and requires the `'unsafe-inline'` Content
       Security Policy directive

--- addon/instance-initializers/anti-clickjacking.js
+++ addon/instance-initializers/anti-clickjacking.js
@@ -3,7 +3,13 @@ export default {
   name: 'anti-clickjacking',
 
   initialize () {
-    if (self !== top) {
+    if (self === top) {
+      const antiClickjack = document.getElementById('antiClickjack')
+
+      if (!antiClickjack) return
+
+      antiClickjack.parentNode.removeChild(antiClickjack)
+    } else {
       top.location = self.location
     }
   }
